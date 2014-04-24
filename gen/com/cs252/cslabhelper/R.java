9177b786510fe750a63a191c9915272b7dac2f7f
Cameron Heskett <Cameron517@gmail.com>
2014-04-24 17:05:17 -0400

    fixed class naming error

--- gen/com/cs252/cslabhelper/R.java
+++ gen/com/cs252/cslabhelper/R.java
@@ -8,6 +8,11 @@
 package com.cs252.cslabhelper;
 
 public final class R {
+    public static final class array {
+        public static final int classArray=0x7f060000;
+        public static final int dayArray=0x7f060002;
+        public static final int timeArray=0x7f060001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,27 +29,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int classList=0x7f080002;
-        public static final int classText=0x7f080005;
-        public static final int csLabHelper=0x7f080000;
-        public static final int nameText=0x7f080001;
-        public static final int nameTextBox=0x7f080003;
-        public static final int sendButton=0x7f080004;
+        public static final int action_settings=0x7f09000b;
+        public static final int classSpinner=0x7f090004;
+        public static final int classText=0x7f090003;
+        public static final int csLabHelper=0x7f090000;
+        public static final int daySpinner=0x7f090008;
+        public static final int dayText=0x7f090006;
+        public static final int nameText=0x7f090001;
+        public static final int nameTextBox=0x7f090002;
+        public static final int sendButton=0x7f090009;
+        public static final int timeSpinner=0x7f090007;
+        public static final int timeText=0x7f090005;
+        public static final int viewAllButon=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
+        public static final int Class=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int day=0x7f050007;
         public static final int hello_world=0x7f050002;
         public static final int insertName=0x7f050004;
         public static final int name=0x7f050003;
         public static final int okay=0x7f050005;
+        public static final int time=0x7f050008;
+        public static final int viewAll=0x7f050009;
     }
     public static final class style {
         /** 
@@ -68,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
