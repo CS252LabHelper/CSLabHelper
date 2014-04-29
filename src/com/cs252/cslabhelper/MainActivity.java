a8f36893ed9860e57402cfd3ea7306937ed76a59
dkirschl <denverkirschling@pal-nat184-097-094.itap.purdue.edu>
2014-04-29 17:35:57 -0400

    Basic On-click

--- src/com/cs252/cslabhelper/MainActivity.java
+++ src/com/cs252/cslabhelper/MainActivity.java
@@ -1,18 +1,30 @@
 package com.cs252.cslabhelper;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 	public static ClassDataSource datasource;
-
+	Button send;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	/*	Button okayButton = (Button)findViewById(R.id.sendButton);
+		okayButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});*/
+		addSendListener();
 		setContentView(R.layout.activity_main);
 		datasource = new ClassDataSource(this);
 		datasource.open();
@@ -21,6 +33,18 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	public void addSendListener()
+	{
+		send = (Button)findViewById(R.id.sendButton);
+		send.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v)
+			{
+				TextView tv = (TextView)findViewById(R.id.nameTextBox);
+				tv.setText((CharSequence) findViewById(R.id.nameText));
+			}
+		});
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
